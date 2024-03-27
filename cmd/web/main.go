a4325905639abbeb9dfade7b9eab13a582829c57
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-03-26 21:14:41 -0400

    :lipstick: add static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
 func main() {
     mux := http.NewServeMux()
+
+    fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+    mux.Handle("/static", http.NotFoundHandler())
+    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
     mux.HandleFunc("GET /{$}", home)
     mux.HandleFunc("GET /snippet/view/{id}", snippetView)
     mux.HandleFunc("GET /snippet/create", snippetCreate)
@@ -16,4 +22,34 @@ func main() {
     
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+    fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+    f, err := nfs.fs.Open(path)
+
+    if err != nil {
+        return nil, err
+    }
+
+    s, err := f.Stat()
+
+    if s.IsDir() {
+        index := filepath.Join(path, "index.html")
+
+        if _, err := nfs.fs.Open(index); err != nil {
+            closeErr := f.Close()
+
+            if closeErr != nil {
+                return nil, closeErr
+            }
+
+            return nil, err
+        }
+    }
+
+    return f, nil
 }
