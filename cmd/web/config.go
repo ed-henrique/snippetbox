7bae9832b3d128c8e1123d1173837f47b80e52be
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-03-27 14:41:23 -0400

    :card_file_box: add database connection

--- cmd/web/config.go
+++ cmd/web/config.go
@@ -3,12 +3,14 @@ package main
 import "flag"
 
 type config struct {
+	dsn       string
 	addr      string
 	staticDir string
 	debug     bool
 }
 
 func getConfig() (cfg config) {
+	flag.StringVar(&cfg.dsn, "dsn", "web:12345678@/snippetbox?parseTime=true", "MariaDB data source name")
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.BoolVar(&cfg.debug, "debug", false, "Debug mode")
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
+	db *sql.DB
 	config config
 	logger *slog.Logger
 }
@@ -24,14 +28,41 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, loggerHandlerOptions))
 
+	db, err := openDB(*&cfg.dsn)
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
 	app := &application{
+		db: db,
 		config: cfg,
 		logger: logger,
 	}
 
 	logger.Info("starting server", slog.String("addr", cfg.addr))
 
-	err := http.ListenAndServe(cfg.addr, app.routes())
+	err = http.ListenAndServe(cfg.addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
