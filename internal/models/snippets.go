b1c61d2c554e0ece2d91ba9a29f7f1990041d8b9
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-03-29 08:14:45 -0400

    :card_file_box: adds  query by id for snippet model

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,46 +2,69 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
 type Snippet struct {
-  ID int
-  Title string
-  Content string
-  Created time.Time
-  Expires time.Time
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
 }
 
 type SnippetModel struct {
-  DB *sql.DB
+	DB *sql.DB
 }
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-  result, err := m.DB.Exec(
-    "INSERT INTO snippets (title, content, created, expires) VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))",
-    title,
-    content,
-    expires,
-  )
+	result, err := m.DB.Exec(
+		`INSERT INTO snippets (title, content, created, expires)
+		 VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`,
+		title,
+		content,
+		expires,
+	)
 
-  if err != nil {
-    return 0, err
-  }
+	if err != nil {
+		return 0, err
+	}
 
-  id, err := result.LastInsertId()
+	id, err := result.LastInsertId()
 
-  if err != nil {
-    return 0, err
-  }
+	if err != nil {
+		return 0, err
+	}
 
-  return int(id), err
+	return int(id), err
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-  return Snippet{}, nil
+	var s Snippet
+
+	if err := m.DB.QueryRow(
+		`SELECT id, title, content, created, expires
+		 FROM snippets
+		 WHERE expires > UTC_TIMESTAMP() AND id = ?`,
+		id,
+	).Scan(
+		&s.ID,
+		&s.Title,
+		&s.Content,
+		&s.Created,
+		&s.Expires,
+	); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		}
+
+		return Snippet{}, err
+	}
+
+	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-  return nil, nil
+	return nil, nil
 }
